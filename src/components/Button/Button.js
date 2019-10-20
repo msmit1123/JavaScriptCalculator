93e1dc89f5d3c5fbddea0b7c8b1085607b08a315
Mikie Smit <msmit1123@gmail.com>
2019-10-20 07:21:02 -0500

    cleaned up Button component code

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,39 +1,15 @@
-/**
- * Import Node Modules
- */
-import React from 'react'
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
-/**
- * Import other Dependencies
- */
-//CSS
-import './Button.scss'
+import './Button.scss';
 
-function Button(props){
-    var handleClick = (event) => props.onClick(event);
-
-    return (
-        <div className={props.classes} id={props.id} onClick={handleClick} value={props.value}>
-            {props.children}
-        </div>
-    )
+function Button({ children, ...props }) {
+  return <div {...props}>{children}</div>;
 }
 
-
-/**
- *  Define component PropTypes
-*/
 Button.propTypes = {
-    children: PropTypes.node,
-    onClick: PropTypes.func.isRequired,
-}
+  children: PropTypes.node,
+  onClick: PropTypes.func.isRequired
+};
 
-
-/**
- *  Export the component
- */
-export default Button
-
-
-
+export default Button;
