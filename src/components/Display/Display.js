838b562754a79b9a09e71fedd403c63daf127e7b
Mikie Smit <msmit1123@gmail.com>
2019-10-20 18:27:40 -0500

    cleaned up Display Component CSS

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,50 +1,33 @@
-/**
- * Import Node Modules
- */
-import React from 'react'
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
-/**
- * Import other Dependencies
- */
-//CSS
-import './Display.scss'
+import './Display.scss';
 
-function Display(props){
-    return (
-        <div id="calculator-top">
-            <div id="display-container">
-                <div className="text last-entry">&nbsp;{props.lastEntryText}</div>
-                <div className="text last-answer">&nbsp;{props.lastAnswer}</div>
-                <div className="text working"><span>{props.content}</span></div>
-                {/* Create mock display for FreeCodeCamp's grading Script */}
-                <div id="display">
-                    {props.content === "" ? props.lastAnswer : props.content}
-                </div>
-            </div>
-        </div>
-    )
+function Display(props) {
+  return (
+    <div className='display'>
+      <div className='display__text-line display__text-line--last-entry'>
+        &nbsp;{props.lastEntryText}
+      </div>
+      <div className='display__text-line display__text-line--last-answer'>
+        &nbsp;{props.lastAnswer}
+      </div>
+      <div className='display__text-line display__text-line--working-entry'>
+        <span>{props.content}</span>
+      </div>
+      {/* Create mock display for FreeCodeCamp's grading Script */}
+      <div id='display'>
+        {props.content === '' ? props.lastAnswer : props.content}
+      </div>
+    </div>
+  );
 }
 
-
-/**
- *  Define component PropTypes
-*/
 Display.propTypes = {
-    lastEntryText: PropTypes.string,
-    lastAnswer: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ]),
-    content: PropTypes.string,
-    children: PropTypes.node,
-}
+  lastEntryText: PropTypes.string,
+  lastAnswer: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  content: PropTypes.string,
+  children: PropTypes.node
+};
 
-
-/**
- *  Export the component
- */
-export default Display
-
-
-
+export default Display;
