9f9a71665507d7924416fb17a23754d7ca15022a
Mikie Smit <msmit1123@gmail.com>
2019-10-22 08:16:20 -0500

    removed header and development notes for production version

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
-/**
- * import node modules
- */
 import React from 'react';
 import './App.scss';
 
-/**
- * Import Components
- */
-import Header from './containers/Header/Header.js';
 import Calculator from './containers/Calculator/Calculator.js';
-import DevelopmentNotes from './containers/DevelopmentNotes/DevelopmentNotes.js';
 
-
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      isViewNotes:false,
-    }
-    this.viewApp = this.viewApp.bind(this);
-    this.viewNotes = this.viewNotes.bind(this);
-  }
-
-  viewNotes(){
-    this.setState({ isViewNotes:true })
-  }
-  viewApp() {
-    this.setState({ isViewNotes: false })
-  }
-
-  render(){
-    return (
-      <div className="App">
-        <Header view={ {
-          isViewNotes: this.state.isViewNotes,
-          viewApp: this.viewApp,
-          viewNotes: this.viewNotes
-        } } />
-        <div id="page-content" className={ !this.state.isViewNotes ? "app-view": "development-view" }>
-          {!this.state.isViewNotes && <Calculator />}
-          {this.state.isViewNotes && <DevelopmentNotes />}
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
